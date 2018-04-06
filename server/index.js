bc0af44288806f7fa1f4f40ceeec584deb39310c
Marcello Messori <mmessori@castgroup.it>
2018-04-06 16:29:30 +0200

    [ADD] parsing of the data file

--- server/index.js
+++ server/index.js
@@ -1,11 +1,32 @@
 const app = require('express')()
+      fs = require('fs')
       path = require('path')
-    , PORT = 3000;
+      dataFilePath = `${path.dirname(__dirname)}/samples/data.json`
+    , port = 3000;
 
 app.get('/', (req, res) => {
     res.sendFile(`${path.dirname(__dirname)}/client/index.html`);
 });
 
-app.listen(PORT);
+app.listen(port);
 
-console.log(`Server listening on port ${PORT}`);
+fs.readFile(dataFilePath, (err, data) => {
+    if (err) throw err;
+
+    const samples = JSON.parse(data.toString())
+        , temperatureSamples = samples.filter(sample => {
+              return Object.keys(sample).includes('temperature');
+          })
+        , numTemperatureSamples = temperatureSamples.length
+        , sumTemperatures = temperatureSamples.map(sample => {
+            return sample.temperature;
+          }).reduce((prev, curr) => {
+              return (prev + curr);
+          }, 0)
+        , avgTemperature = sumTemperatures/numTemperatureSamples;
+
+    console.log(`${numTemperatureSamples} temperature samples`);
+    console.log(`average temperature is ${avgTemperature}`);
+});
+
+console.log(`Server listening on port ${port}`);
