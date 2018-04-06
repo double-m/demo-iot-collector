11f31d865da58464001578e72835d8e405d9e50b
Marcello Messori <mmessori@castgroup.it>
2018-04-06 13:35:50 +0200

    [ADD] index.html served by express.js

--- server/index.js
+++ server/index.js
@@ -1,1 +1,11 @@
-console.log('TBD');
+const app = require('express')()
+      path = require('path')
+    , PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.sendFile(`${path.dirname(__dirname)}/client/index.html`);
+});
+
+app.listen(PORT);
+
+console.log(`Server listening on port ${PORT}`);
